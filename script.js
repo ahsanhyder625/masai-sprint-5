c88235838d15449f49398c22550c53765a2cc9fe
Ahsan hyder <39864885+ahsanhyder625@users.noreply.github.com>
2020-07-15 15:48:53 +0530

    js commit

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 alert("hello")
+const api={
+    key:"2d428749461382aec70725c4da65e00c",
+    base: "https://api.openweathermap.org/data/2.5/"
+
+}
+const searchbox=document.querySelector(".search-box");
+searchbox.addEventListener("keypress",setQuery)
